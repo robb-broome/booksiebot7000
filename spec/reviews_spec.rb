a02776842895d6762c34f1d5fb6b83f9b995554d
Robb Broome <robb@teamvoice.com>
2011-03-18 00:00:21 -0500

    Robb. Further test cleanup. Sadly, not finding the action in the app file.

--- spec/reviews_spec.rb
+++ spec/reviews_spec.rb
@@ -2,11 +2,7 @@ require File.dirname(__FILE__) + '/spec_helper'
 
 describe "Booksiebot" do
 
-  def app
-    @app ||= Sinatra::Application
-  end
-
-  it "should respond to /" do
+  it "should respond to /reviews" do
     get '/reviews'
     last_response.should be_ok
   end
