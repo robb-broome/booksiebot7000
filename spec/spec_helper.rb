296e3845b36e35e5c6f8828c0791d258262ac859
Robb Broome <robb@teamvoice.com>
2011-03-17 23:57:31 -0500

    Robb. Cleanup spec_helper, move common stuff there.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,14 +1,19 @@
 require File.join(File.dirname(__FILE__), '..', 'booksiebot.rb')
 
 require 'rack/test'
-require 'test/unit'
 
-class Test::Unit::TestCase
-  include Rack::Test::Methods
+# don't have to have the include in every spec
+RSpec.configure do |conf|
+  conf.include Rack::Test::Methods
+end
+
+def app
+  #@app ||= Sinatra::Application
+  Booksiebot
 end
 
 # set test environment
 set :environment, :test
 # set :run, false
-# set :raise_errors, true
+set :raise_errors, true
 # set :logging, false
